28b02a8eee9330441a5eb4973565e4f84de88ae4
KathanGoda26 <kathangoda46@gmail.com>
2026-08-18 00:51:30 +0530

    Create authentication page boundary

---
+++ frontend/src/pages/auth/index.js
@@ -0,0 +1,4 @@
+export { default as Login } from '@/views/auth/Login'
+export { default as ForgotPassword } from '@/views/auth/ForgotPassword'
+export { default as ResetPassword } from '@/views/auth/ResetPassword'
+export { default as VerifyEmail } from '@/views/auth/VerifyEmail'
